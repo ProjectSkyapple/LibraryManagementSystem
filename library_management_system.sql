5a52bfbcfbfa51fca1f74517396e6c77a332ac0b
ProjectSkyapple <avgeekydev@gmail.com>
2025-04-03 13:37:09 -0500

    Add search query comments

--- library_management_system.sql
+++ library_management_system.sql
@@ -1,3 +1,9 @@
+-- 1. Search and Availability
+/* NOTE: If a book is checked out, the corresponding book_loans.date_in if it exists will be NULL.
+ * If it is checked in, book_loans.date_in will be a non-null value,
+ * or it does not have a corresponding book_loans record.
+ */
+-- TODO: Consider when a book has never been loaned before (there won't be a corresponding book_loans record).
 SELECT book.isbn, book.title, authors.name, book_loans.date_in
 FROM book, book_authors, authors, book_loans
 WHERE book.isbn = book_authors.isbn
